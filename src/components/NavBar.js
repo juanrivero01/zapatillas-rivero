3c8b2f23afe1468e2a1f278477e20718d2cb0c6b
JuanRivero <juanchoorivero@gmail.com>
2022-03-16 17:55:39 -0300

    segundo desafio

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import './NavBar.css'
+import Button from '@mui/material/Button';
+
+function NavBar() {
+    return(
+        <header className='main-header'> 
+            <div className='logo-container'> 
+                <img className= 'logo'
+                    src= "footer-locker.png"
+                />
+            </div> 
+                <ul className= 'navbar'>
+                    <li><Button>Shoes</Button></li>
+                    <li><Button>Clothing</Button></li>
+                    <li><Button>New Releases</Button></li>
+                    <li><Button>Size Guide</Button></li>
+                </ul>
+        </header>
+    )
+}
+
+export default NavBar
